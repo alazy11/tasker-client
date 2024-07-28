677d8fcd38ebddf3bda3a13b96abfd19e76c55a6
alazy11 <alazyalhimeari11@gmail.com>
2024-07-29 02:57:04 +0300

    last mod

--- src/app/[lang]/company/component/drop_menu/space_option/ChangeIcon.jsx
+++ src/app/[lang]/company/component/drop_menu/space_option/ChangeIcon.jsx
@@ -4,7 +4,7 @@
 import DropDownModel from '@/app/[lang]/component/DropDownModel';
 import dynamic from 'next/dynamic';
  import { useEffect, useState } from 'react';
-import { IconPicker } from '@locaop/icon-picker'
+// import { IconPicker } from '@locaop/icon-picker'
 
 const colorIcon = ['#8077f1', '#6a85ff', '#40a6e5', '#3fb1b2', '#64c6a2', '#f9be34', '#e78845', '#dc646a', '#f17ead', '#c580e6', '#bba399', '#595d66'];
 // const backcolorIcon = ['cu-project-avatar__space-bg-purple', 'cu-project-avatar__space-bg-neon-blue', 'cu-project-avatar__space-bg-azure-blue', 'cu-project-avatar__space-bg-teal', 'cu-project-avatar__space-bg-mint', 'cu-project-avatar__space-bg-yellow', 'cu-project-avatar__space-bg-orange', 'cu-project-avatar__space-bg-red', 'cu-project-avatar__space-bg-pink', 'cu-project-avatar__space-bg-violet', 'cu-project-avatar__space-bg-brown', 'cu-project-avatar__space-bg-black'];
@@ -48,7 +48,7 @@ style={{ maxHeight:" calc(100vh - 20px)",
 
                      <div className='icon-container scroll-bar h-full'>
                                  <div className=' h-full'>
-                                 <IconPicker value={value} onChange={(v) => setValue(v)} />
+                                 
                                  </div>
                               </div>
                         
